9340eaa0036fb2568bcae64cb064d4503da748a2
Dohyeon Kim <dohyeon.kim@life.church>
2022-07-01 10:19:21 -0500

    Revert "Use net_http adapter"
    This reverts commit d75dcf45e53a33fd2c63b555453f099162839bf0.

--- lib/rock_rms/client.rb
+++ lib/rock_rms/client.rb
@@ -1,5 +1,4 @@
 require 'faraday'
-require 'faraday/net_http'
 require 'faraday_middleware'
 require 'faraday_middleware/parse_oj'
 
@@ -49,7 +48,6 @@ module RockRMS
       @cookie   = auth["set-cookie"] unless auth.nil?
       @authorization_token = authorization_token
       @consumer = consumer
-      Faraday.default_adapter = :net_http
     end
 
     def delete(path, options = {})
